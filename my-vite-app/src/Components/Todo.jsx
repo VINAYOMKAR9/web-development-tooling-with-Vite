5adea247f66af3720dd59deabcb1096f97d2a030
VINAYOMKAR9 <vinayomkar9@gmail.com>
2024-07-28 14:48:24 +0530

    react

--- my-vite-app/src/Components/Todo.jsx
+++ my-vite-app/src/Components/Todo.jsx
@@ -1,18 +1,43 @@
 import React, { useState } from 'react'
 
-const Todo = () => {
-    let fObj= {id:1,title:'Todo 1',status:false}
-const [text,setText]=useState('')
-const [todo,setTodo]=useState([fObj,])
+const TodoList=({todo,handleToggle,handleDelete})=>{
+    return (<div>      
+        {
+            todo.map((el)=>{
+                return(
+                <div key={el.id}  style={{
+                    backgroundColor:el.status?'green':'orange', 
+                    padding:'3px', 
+                    margin: '4px'}}>
+                    <h2>{el.id}</h2>
+                    <h4>{el.title}</h4>
+                    <b>{el.status?'completed':'un-complete'}</b>
+                    <button onClick={()=>handleToggle(el.id)}> ToogleStatus</button>
+                    <button onClick={()=>handleDelete(el.id)}> ToogleDelete</button>
 
-    console.log(todo)
+
+                </div>
+                )
+            })
+        }
+        
+        </div>
+)
+}
+    
+const Todo = () => {
+    // let fObj= {id:1,title:'Todo 1',status:false}
+const [text,setText]=useState('')
+const [todo,setTodo]=useState([{id:1,title:'Todo 1',status:false},])
+
+    // console.log(todo)
     const handleChange=(event)=>{
         let val = event.target.value
         setText(val)
     }
     const handleClick=()=>{
         let obj={
-            id:Todo.length+1,
+            id:todo.length + 1,
             title:text,
             status:false
         }
@@ -20,11 +45,29 @@ const [todo,setTodo]=useState([fObj,])
         setText('')
     }
 
+    const handleToggle=(id)=>{
+        let toggleData = todo.map((el)=>el.id == id?{...el,status:!el.status}:el)
+        setTodo(toggleData)
+        
+
+    }
+
+
+    const handleDelete=(id)=>{
+        let filteredData= todo.filter((el)=>el.id != id)
+        setTodo(filteredData)
+
+    }
+
 
   return (<div>
    <h1>Todo</h1>
    <input type='text' placeholder='enter task' value={text} onChange={handleChange}/>
    <button onClick={handleClick}>Add</button>
+
+   <div>
+    <TodoList todo={todo} handleToggle={handleToggle} handleDelete={handleDelete} />
+   </div>
 
     </div>
   )
